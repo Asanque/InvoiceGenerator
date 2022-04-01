756f772e3c5243be4a35920c6eb6f5c7ec392b3a
Asanque <kis.gergely93@gmail.com>
2022-04-02 01:37:05 +0200

    implement using data from table instead of hard coded values

--- WebApplication1/ShopData.cs
+++ WebApplication1/ShopData.cs
@@ -4,9 +4,9 @@ namespace HaviSzamla
 {
     public class ShopData
     {
-        public static List<string> ShopNameList { get; } = new () {"name1", "name2", "name3", "name4", "name5", "name6", "name7", "name8", "name9", "name10", "name11", "name12", "name13", "name14", "name15", "name16", "name17", "name18", "name19", "name20", "name21", "name22" };
-        public static List<string> ShopAddressList { get; } = new () { "address1", "address2", "address3", "address4", "address5", "address6", "address7", "address8", "address9", "address10", "address11", "address12", "address13", "address14", "address15", "address16", "address17", "address18", "address19", "address20", "address21", "address22" };
-        public static List<string> ShopVatsList { get; } = new () { "VAT1", "VAT2", "VAT3", "VAT4", "VAT5", "VAT6", "VAT7", "VAT8", "VAT9", "VAT10", "VAT11", "VAT12", "VAT13", "VAT14", "VAT15", "VAT16", "VAT17", "VAT18", "VAT19", "VAT20", "VAT21", "VAT22" };
+        public static List<string> ShopNameList { get; } = new();
+        public static List<string> ShopAddressList { get; } = new ();
+        public static List<string> ShopVatList { get; } = new ();
 
         public string Month { get; }
         public int WeeksInMonth { get; }
@@ -26,6 +26,19 @@ namespace HaviSzamla
         public static ShopData GetInstance()
         {
             return _instance;
+        }
+
+        public void AddToNameList(string name)
+        {
+            ShopNameList.Add(name);
+        }
+        public void AddToAddressList(string address)
+        {
+            ShopAddressList.Add(address);
+        }
+        public void AddToVatList(string vat)
+        {
+            ShopVatList.Add(vat);
         }
     }
 }
